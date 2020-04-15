e1d564ed2ee34947214f61b05797f804501170c4
Jakub Schier <jakub.schier@mojemana.cz>
2020-04-15 20:44:24 +0200

    Checking the return codes

--- api_calls/get_info_json.py
+++ api_calls/get_info_json.py
@@ -1,22 +1,21 @@
 import aiohttp
-from aiohttp.client_exceptions import ClientConnectionError
-import io
 from json import JSONDecodeError
 import json
 from api_calls.image_builder import get_images
 import random
+
 
 class Kills:
 	def __init__(self):
 		self.already_displayed = []
 		self.kill_messages = {
 			1: "Za Moc a Slavu!",
-		    2: "Za vetsi slavu gildy!",
-		    3: "Chvalte Brychtezena!",
-		    3: "Smrt nepratelum!",
-		    4: "Baf!",
-		    5: "Smrt Msicim Lidem!"
-		                      }
+			2: "Za vetsi slavu gildy!",
+			3: "Chvalte Brychtezena!",
+			3: "Smrt nepratelum!",
+			4: "Baf!",
+			5: "Smrt Msicim Lidem!"
+		}
 		self.victim_messages = {
 			1: "Slabi nebudou diktovat!",
 			2: "Takova potupa!",
@@ -30,18 +29,20 @@ class Kills:
 	@staticmethod
 	async def fetch(session, url):
 		async with session.get(url) as response:
-			return await response.text()
+			try:
+				assert response.status == 200
+			except AssertionError:
+				print(f"Response recieved: {response.status}")
+
+			return await response.read()
 
 	async def main(self):
 		async with aiohttp.ClientSession() as session:
+			html = await Kills.fetch(session,
+			                         'https://gameinfo.albiononline.com/api/gameinfo/events?limit=51&offset=0'
+			                         )
 			try:
-				html = await Kills.fetch(session,
-				                         'https://gameinfo.albiononline.com/api/gameinfo/events?limit=51&offset=0')
-			except:
-				print("Client exception error, continuing...")
-				pass
-			try:
-				json_out = json.loads(html)
+				json_out = json.loads(html.decode("utf-8"))
 				for i in json_out:
 					if i["Killer"]["GuildId"] == self.guild_id and i["EventId"] not in self.already_displayed:
 						# TODO: this all is a duplicit code, move to function or find other way to remove it
@@ -55,17 +56,17 @@ class Kills:
 						stats["Gdy_se_to_stalo"] = date + " " + time
 						self.already_displayed.append(i["EventId"])
 						image = get_images(_items,
-						                  i["Killer"]["Name"],
-						                  i["Victim"]["Name"],
-						                  stats["Gdy_se_to_stalo"],
-						                  i["Killer"]["GuildName"],
-						                  i["Victim"]["GuildName"],
-						                  i["Killer"]["AverageItemPower"],
-						                  i["Victim"]["AverageItemPower"],
-						                  i["TotalVictimKillFame"],
-						                  self.kill_messages[random.randint(1,5)],
-						                  i["Participants"]
-						                  )
+						                   i["Killer"]["Name"],
+						                   i["Victim"]["Name"],
+						                   stats["Gdy_se_to_stalo"],
+						                   i["Killer"]["GuildName"],
+						                   i["Victim"]["GuildName"],
+						                   i["Killer"]["AverageItemPower"],
+						                   i["Victim"]["AverageItemPower"],
+						                   i["TotalVictimKillFame"],
+						                   self.kill_messages[random.randint(1, 5)],
+						                   i["Participants"]
+						                   )
 						return image
 
 					elif i["Victim"]["GuildId"] == self.guild_id and i["EventId"] not in self.already_displayed:
@@ -87,7 +88,7 @@ class Kills:
 						                   i["Killer"]["AverageItemPower"],
 						                   i["Victim"]["AverageItemPower"],
 						                   i["TotalVictimKillFame"],
-						                   self.victim_messages[random.randint(1,5)],
+						                   self.victim_messages[random.randint(1, 5)],
 						                   i["Participants"]
 						                   )
 						return image
